197116d9bb0ebb935a845e5bf08ff461e4a31832
attx <attx@localhost>
2023-09-23 12:05:37 +0200

    deb1.h

---
+++ third_party/cmsis/CMSIS/NN/Include/deb1.h
@@ -0,0 +1,20 @@
+#ifndef DEB1_H
+#define DEB1_H
+
+extern volatile int volatile_data[32];
+extern volatile int cnt_data[32];
+
+#define DBG_PT1 1
+#define DBG_PT2 2
+#define DBG_PT3 3
+#define DBG_PT4 4
+#define DBG_PT5 5
+#define DBG_PT6 6
+
+#define CNT_PT1 0
+#define CNT_PT2 1
+#define CNT_PT3 2
+
+#endif
+
+
